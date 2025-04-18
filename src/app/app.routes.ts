113824acff27a623b66ad7463fd6863aa8d191df
unknown <mnunez@saargo.com>
2025-04-18 10:08:44 -0400

    feature: se agrega componente features/circulares

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@
 import { Routes } from '@angular/router';
 import { MuralComponent } from './features/mural/mural.component';
 import { AdminComponent } from './features/admin/admin.component';
+import { CircularesComponent } from './features/circulares/circulares.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'mural', pathMatch: 'full' },
   { path: 'mural', component: MuralComponent }
+  { path: 'circulares', component: CircularesComponent }
   { path: 'administracion', component: AdminComponent }
 ];
---
+++ src/app/features/circulares/circulares.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circulares',
+  imports: [],
+  templateUrl: './circulares.component.html',
+  styleUrl: './circulares.component.css'
+})
+export class CircularesComponent {
+
+}
