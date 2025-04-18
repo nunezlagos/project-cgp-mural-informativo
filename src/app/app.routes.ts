5046bbe8ef8ba3f860e8719c6ad7331c36c1495e
unknown <mnunez@saargo.com>
2025-04-18 10:08:07 -0400

    feature: se agrega componente features/admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
 import { MuralComponent } from './features/mural/mural.component';
+import { AdminComponent } from './features/admin/admin.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'mural', pathMatch: 'full' },
   { path: 'mural', component: MuralComponent }
+  { path: 'administracion', component: AdminComponent }
 ];
