b17cd3ba0498a162735f9f6b0bf420efa39f147a
unknown <mnunez@saargo.com>
2025-04-18 09:50:14 -0400

    feature: se añade logica router-outlet  para manejo de vistas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
-// app.component.ts
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';  // Importa RouterModule
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { MuralComponent } from './features/mural/mural.component';
-
-
-
 
 @Component({
   selector: 'app-root',
   standalone: true,  // Marca el AppComponent como standalone
-  imports: [NavbarComponent, MuralComponent, FooterComponent],  // Importa los componentes standalone
+  imports: [NavbarComponent, FooterComponent, RouterModule],  // Importa RouterModule para manejar las rutas
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { MuralComponent } from './features/mural/mural.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'mural', pathMatch: 'full' },
+  { path: 'mural', component: MuralComponent }
+];
