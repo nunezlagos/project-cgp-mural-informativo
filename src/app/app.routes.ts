5a1e927b2665a21b498965392c62950118f9de21
Mauricio nuñez <mauricio@nunezlagos.cl>
2025-04-22 22:07:53 -0400

    feature: nosotros y actas (maquetado)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@
 import { Routes } from '@angular/router';
 import { MuralComponent } from './features/mural/mural.component';
 import { AdminComponent } from './features/admin/admin.component';
-import { CircularesComponent } from './features/circulares/circulares.component';
+import { ActasComponent } from './features/actas/actas.component';
+import { NosotrosComponent } from './features/nosotros/nosotros.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: MuralComponent },  // Ahora esta es la raíz directamente
-  { path: 'circulares', component: CircularesComponent },
+  { path: '', component: MuralComponent },
+  { path: 'actas', component: ActasComponent },
+  { path: 'nosotros', component: NosotrosComponent },
   { path: 'administracion', component: AdminComponent, canActivate: [AuthGuard] }
 ];
---
+++ src/app/features/actas/actas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actas',
+  standalone:true,
+  templateUrl: './actas.component.html',
+  styleUrl: './actas.component.css'
+})
+export class ActasComponent {
+
+}
